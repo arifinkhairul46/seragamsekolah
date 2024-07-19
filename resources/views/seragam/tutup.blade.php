ee93e955e6aac4dd55cef2c0f062346917b91dbd
khairul <arifinkhairul46@gmail.com>
2024-07-20 06:35:54 +0700

    tutup home

--- resources/views/seragam/tutup.blade.php
+++ resources/views/seragam/tutup.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Mohon Maaf Belum dapat di akses, baru dibuka tanggal 16 Juli 2024 </h1>
+    <h1> Mohon Maaf PO Seragam Rabbani sudah ditutup. Nantikan informasi selanjutnya. Terimakasih  </h1>
 @endsection
